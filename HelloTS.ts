15ff315e5f7ef2aa2387615bf924d9a2cbbf57a1
Anshul <43234382+AnshulAggarwal15@users.noreply.github.com>
2020-02-26 23:53:39 +0530

    Create HelloTS.ts

---
+++ HelloTS.ts
@@ -0,0 +1,7 @@
+//Hello TypeScript program
+
+export {} 
+//does not export any file and treats program as a module and not as script
+let message = 'Hello TypeScript';
+console.log(message);
+//Prints output on screen
