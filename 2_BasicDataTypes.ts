8d8a794826cb0432bf72a7c500594c3da5e1b606
Anshul <43234382+AnshulAggarwal15@users.noreply.github.com>
2020-02-29 23:04:54 +0530

    Create 2_BasicDataTypes.ts

---
+++ 2_BasicDataTypes.ts
@@ -0,0 +1,18 @@
+//bool
+//number
+//string
+
+
+//syntax:
+let var_name : var_type = value; //can be a constant also
+
+let isBegin : boolean = true;
+
+let num : number = 10;
+
+let name : string = 'ItsYoBoi';
+
+let sent : string = `Hello, I am ${name}
+I will be a pro`;
+
+console.log(sent);
