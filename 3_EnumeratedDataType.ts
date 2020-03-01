ad9d2d5109f56ab2f6bb0f0919ff8bc6c03fdcc9
Anshul <43234382+AnshulAggarwal15@users.noreply.github.com>
2020-03-01 23:16:11 +0530

    Create 3_EnumeratedDataType.ts

---
+++ 3_EnumeratedDataType.ts
@@ -0,0 +1,14 @@
+//enum is a fun way to give name to numbers
+//example-
+
+enum color{red,green,yellow}
+
+let c:color = color.green
+
+console.log(c);
+
+enum color{red=5,green,yellow}
+
+let d:color = color.green
+
+console.log(d);
