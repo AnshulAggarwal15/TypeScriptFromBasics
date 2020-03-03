af63cb8d5ed4a233e95a878b226e130e94e4db23
Anshul <43234382+AnshulAggarwal15@users.noreply.github.com>
2020-03-03 23:43:59 +0530

    Create 5_UnknownDataType.ts

---
+++ 5_UnknownDataType.ts
@@ -0,0 +1,13 @@
+let a = 1;
+console.log(a);
+
+let b : unknown  = 5;
+
+console.log(b);
+
+b = 'rgr';
+
+console.log(b);
+
+//It is similar to "any" data type but any function named as b will be invalid here and compiler will show errror 
+//hence efficiency of code is much better
