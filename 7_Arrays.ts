9b3dc102d8f26186b54a920bfb3ddee4a2dfea53
Anshul <43234382+AnshulAggarwal15@users.noreply.github.com>
2020-03-06 22:47:27 +0530

    Create 7_Arrays.ts

---
+++ 7_Arrays.ts
@@ -0,0 +1,13 @@
+//ARRAY DECLARATION
+
+let list1 : number[] = [1,2,3];
+let list2 : array<number> = [1,2,3];
+
+//In TypeScript we can have an array with data of different data types
+//for declaring arrays of 2 data types
+//we can use tuple
+//example-
+
+let person : [string,number] = [Anshul,1];
+
+//tuple can have limited no of values and order also has to be same
