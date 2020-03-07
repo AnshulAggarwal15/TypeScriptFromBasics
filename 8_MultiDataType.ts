5959fb99f06086b97d6d99f1e2c4420e9117033e
Anshul <43234382+AnshulAggarwal15@users.noreply.github.com>
2020-03-07 23:22:36 +0530

    Create 8_MultiDataType.ts

---
+++ 8_MultiDataType.ts
@@ -0,0 +1,9 @@
+let multitype = number|boolean;
+multitype = 12;
+multitype = false;
+
+let anytype = any;
+anytype = 12;
+anytype = false;
+
+//difference between multi and any is same as between any and unknown
